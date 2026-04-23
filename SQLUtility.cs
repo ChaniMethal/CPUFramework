db171f746e2033a4391f5b42501cd7a3697d7baf
ChaniMethal <chani.silberberg@gmail.com>
2026-04-23 03:48:01 -0400

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            //DisplayMessage("Conn Status", conn.State.ToString());
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
