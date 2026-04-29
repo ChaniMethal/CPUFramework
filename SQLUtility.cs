00f7d27b6f497a4414706c8c73e16cfc4115447b
ChaniMethal <chani.silberberg@gmail.com>
2026-04-29 14:28:26 -0400

    SQLUtility

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -10,17 +10,47 @@ namespace CPUFramework
         public static string ConnectionString = "";
         public static DataTable GetDataTable(string sqlstatement)
         {
+            Debug.Print(sqlstatement);
             DataTable dt = new();
             SqlConnection conn = new();
             conn.ConnectionString = ConnectionString;
             conn.Open();
-            //DisplayMessage("Conn Status", conn.State.ToString());
+            
             var cmd = new SqlCommand();
             cmd.Connection = conn;
             cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+
+            SetAllColumnsAllowNull(dt);
+
             return dt;
+        }
+
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0) {
+                if(dt.Rows[0][0] != DBNull.Value)
+                {
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+            }
+            return n;
+        }
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach(DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
         }
         public static void DebugPrintDataTable(DataTable dt)
         {
